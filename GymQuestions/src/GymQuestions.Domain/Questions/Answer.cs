00557948d83200deb1b09bef4ca28b469f861d26
DDInside4life <roman_luk99@mail.ru>
2026-03-09 20:24:00 +0300

    done with domain models

---
+++ GymQuestions/src/GymQuestions.Domain/Questions/Answer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymQuestions.Domain.Questions
+{
+    public class Answer
+    {
+        //// Конструктор 
+        //public Answer(Guid userId, string text, Question question)
+        //{
+        //    UserId = userId;
+        //    Text = text;
+        //    Question = question;
+        //}
+
+        public Guid Id { get; set; }
+        public required Guid UserId { get; set; }
+        public required string Text { get; set; } = string.Empty;
+
+        // Ключевое слово required - при создании объекта,
+        // необходимо обязательно заполнить данные
+        public required Question Question { get; set; }
+        public List<Guid> Comments { get; set; } = [];
+    }
+}
---
+++ GymQuestions/src/GymQuestions.Domain/Questions/Question.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymQuestions.Domain.Questions
+{
+    public class Question
+    {
+        public Guid Id { get; set; }
+
+        public required string Title { get; set; } = string.Empty;
+        public required string Text { get; set; } = string.Empty;
+
+        // Связь с другим модулем User через Id
+        public required Guid UserId { get; set; }
+        public Guid? ScreenshotId { get; set; }
+
+        // List of answers
+        public List<Answer> Answers { get; set; } = [];
+
+        // nullable свойство
+        // В данном случае прямая ссылка допустима тк Answer и Question в одном модуле
+        public Answer? Solution { get; set; }
+
+        // Ссылка на теги в виде списка Guid
+        public List<Guid> Tags { get; set; } = [];
+
+        // Комментарии в виде списка Guid
+        //public List<Guid> Comments { get; set; } = [];
+    }
+
+   
+}
+
