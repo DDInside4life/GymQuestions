b4d95584463e0f460b52249c4e15bff953c65118
DDInside4life <roman_luk99@mail.ru>
2026-03-22 17:25:58 +0300

    done with template for QuestionsController.cs

---
+++ GymQuestions/src/GymQuestions.Contracts/AddAnswerDto.cs
@@ -0,0 +1,3 @@
+namespace GymQuestions.Contracts;
+
+public record AddAnswerDto(Guid UserId, string Text);
---
+++ GymQuestions/src/GymQuestions.Contracts/GetQuestionsDto.cs
@@ -0,0 +1,3 @@
+namespace GymQuestions.Contracts;
+
+public record GetQuestionsDto(string Search, Guid[] TagIds, int Page, int PageSize);
